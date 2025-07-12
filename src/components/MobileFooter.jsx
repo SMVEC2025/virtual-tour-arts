ca698b084dd3c376991b576838c8e7511284e349
SMVEC2025 <webdeveloper@smvec.ac.in>
2025-07-12 14:29:39 +0530

    changes

--- src/components/MobileFooter.jsx
+++ src/components/MobileFooter.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import { FaAngleLeft,FaAngleRight } from "react-icons/fa6";
 import { FaVrCardboard } from "react-icons/fa6";
+import { GrMapLocation } from "react-icons/gr";
+import { TfiGallery } from "react-icons/tfi";
 
 function MobileFooter() {
   return (
@@ -8,6 +10,8 @@ function MobileFooter() {
       <div className='container'>
         <button><FaAngleLeft/></button>
         <button><FaVrCardboard/></button>
+        <button><GrMapLocation/></button>
+        <button><TfiGallery/></button>
         <button><FaAngleRight/></button>
       </div>
     </div>
