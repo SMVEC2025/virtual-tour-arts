a799cc5f682bc4278d4ff7180612210fbf6eca23
SMVEC2025 <webdeveloper@smvec.ac.in>
2025-07-14 12:12:53 +0530

    changes

--- src/components/viewer.jsx
+++ src/components/viewer.jsx
@@ -9,6 +9,20 @@ const Viewer = () => {
   const [aframeReady, setAframeReady] = useState(false);
 
   useEffect(() => {
+    // Set the initial image when imageData is available
+    if (imageData && imageData.length > 0 && !currentImage) {
+      try {
+        setCurrentImage(imageData[0]);
+      } catch (error) {
+        console.error("Error setting initial image:", error);
+        setTimeout(() => {
+          setIsLoadingImage(false);
+        }, 500);
+      }
+    }
+  }, [imageData, currentImage, setCurrentImage, setIsLoadingImage]); // Depend on imageData, currentImage, etc.
+
+  useEffect(() => {
     const sceneEl = sceneRef.current?.el;
 
     if (!sceneEl) return;
@@ -16,85 +30,70 @@ const Viewer = () => {
     const handleSceneLoaded = () => {
       console.log('A-Frame scene is loaded and ready!');
       setAframeReady(true);
-
-      if (!AFRAME.components['auto-rotate-camera']) {
-        AFRAME.registerComponent('auto-rotate-camera', {
-          schema: {
-            speed: { type: 'number', default: -4 },
-            enabled: { type: 'boolean', default: true },
-          },
-          init: function () {
-            const stopRotation = () => {
-              this.data.enabled = false;
-            };
-            this.el.sceneEl.addEventListener('mousedown', stopRotation);
-            this.el.sceneEl.addEventListener('touchstart', stopRotation);
-          },
-          tick: function (time, delta) {
-            if (!this.data.enabled) return;
-            const rotation = this.el.getAttribute('rotation');
-            const newRotationY = rotation.y + (this.data.speed * (delta / 1000));
-            this.el.setAttribute('rotation', {
-              x: rotation.x,
-              y: newRotationY,
-              z: rotation.z
-            });
-          }
-        });
-      }
-
     };
 
-    if (!isMobile) { sceneEl.addEventListener('loaded', handleSceneLoaded); }
+    // Add event listener once
+    sceneEl.addEventListener('loaded', handleSceneLoaded);
 
-    return () => {
-      if (!isMobile) { sceneEl.addEventListener('loaded', handleSceneLoaded); }
-    };
-  }, []);
-
-  useEffect(() => {
-    try {
-      setCurrentImage(imageData[0]);
-    } catch (error) {
-      setTimeout(() => {
-        setIsLoadingImage(false);
-      }, 500);
+    // Register auto-rotate component only if not mobile
+    if (!isMobile && !AFRAME.components['auto-rotate-camera']) {
+      AFRAME.registerComponent('auto-rotate-camera', {
+        schema: {
+          speed: { type: 'number', default: -4 },
+          enabled: { type: 'boolean', default: true },
+        },
+        init: function () {
+          const stopRotation = () => {
+            this.data.enabled = false;
+          };
+          this.el.sceneEl.addEventListener('mousedown', stopRotation);
+          this.el.sceneEl.addEventListener('touchstart', stopRotation);
+        },
+        tick: function (time, delta) {
+          if (!this.data.enabled) return;
+          const rotation = this.el.getAttribute('rotation');
+          const newRotationY = rotation.y + (this.data.speed * (delta / 1000));
+          this.el.setAttribute('rotation', {
+            x: rotation.x,
+            y: newRotationY,
+            z: rotation.z
+          });
+        }
+      });
     }
 
-
-  }, []);
-
+    return () => {
+      // Clean up event listener
+      sceneEl.removeEventListener('loaded', handleSceneLoaded);
+    };
+  }, [isMobile]); // Re-run if isMobile changes
 
   return (
     <div className='tsv-main'>
       {isLoadingImage && <PreLoader />}
 
       <Scene ref={sceneRef} vr-mode-ui="enabled: true">
-
         <a-assets timeout="30000">
           {imageData.map((data) => (
             <img key={data.id} id={`img-${data.id}`} src={data.image} alt={data.name} crossOrigin="anonymous" />
           ))}
         </a-assets>
+
         {aframeReady && currentImage && (
           <a-sky src={`#img-${currentImage.id}`}></a-sky>
         )}
 
-        <Entity auto-rotate-camera="speed: -4">
-          <a-camera></a-camera>
-        </Entity>
+        {/* Conditionally render auto-rotate camera */}
+        {!isMobile ? (
+          <Entity auto-rotate-camera="speed: -4">
+            <a-camera></a-camera>
+          </Entity>
+        ) : (
+          <Entity>
+            <a-camera></a-camera>
+          </Entity>
+        )}
       </Scene>
-      {/* <Scene ref={sceneRef}>
-        <a-assets timeout="30000">
-          {imageData.map((data) => (
-            <img key={data.id} id={`img-${data.id}`} src={data.image} alt={data.name} crossOrigin="anonymous" />
-          ))}
-        </a-assets>
-        {currentImage && <a-sky src={currentImage.image}></a-sky>}
-        <a-camera user-was-moved="true"></a-camera>
-      </Scene> */}
-
-
     </div>
   );
 };
